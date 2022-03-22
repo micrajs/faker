f70328f156330fbbb5580d17f47eeb078215d9cf
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-21 19:25:42 -0500

    feat: created alphaNumeric extension

---
+++ extensions/alphaNumeric.ts
@@ -0,0 +1,29 @@
+declare global {
+  namespace Faker {
+    interface Extensions {
+      alphaNumeric: (length?: number) => string;
+    }
+  }
+}
+
+export function alphaNumeric(fake: Faker.Instance) {
+  return (length = 10): string => {
+    const maxLength = Math.pow(2, 20);
+    if (length >= maxLength) {
+      length = maxLength;
+    }
+
+    const chars = [
+      () => String.fromCharCode(fake.number(48, 57)),
+      () => String.fromCharCode(fake.number(65, 90)),
+      () => String.fromCharCode(fake.number(97, 122)),
+    ];
+
+    let returnString = '';
+    for (let i = 0; i < length; i++) {
+      returnString += chars[fake.number(chars.length - 1)]();
+    }
+
+    return returnString;
+  };
+}
