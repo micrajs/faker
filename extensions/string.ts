602571fa57b701509ef6a63244f635e2c34b30d5
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-21 19:27:12 -0500

    feat: created string extension

---
+++ extensions/string.ts
@@ -0,0 +1,23 @@
+declare global {
+  namespace Faker {
+    interface Extensions {
+      string: (length?: number) => string;
+    }
+  }
+}
+
+export function string(fake: Faker.Instance) {
+  return (length = 10): string => {
+    const maxLength = Math.pow(2, 20);
+    if (length >= maxLength) {
+      length = maxLength;
+    }
+
+    let returnString = '';
+    for (let i = 0; i < length; i++) {
+      returnString += String.fromCharCode(fake.number(33, 125));
+    }
+
+    return returnString;
+  };
+}
