739c933e19f07d1c4d67dd9b4454bdff1b62d6af
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-21 19:26:46 -0500

    feat: created oneOf extension

---
+++ extensions/oneOf.ts
@@ -0,0 +1,20 @@
+declare global {
+  namespace Faker {
+    interface Extensions {
+      oneOf: <T>(...args: (T | Faker.Generator<T>)[]) => T;
+    }
+  }
+}
+
+export function oneOf(fake: Faker.Instance) {
+  return <T>(...args: (T | ((fake: Faker.Instance) => T))[]): T => {
+    const index = fake.number(args.length - 1);
+    const arg = args[index];
+
+    if (typeof arg === 'function') {
+      return (arg as Faker.Generator<T>)(fake);
+    }
+
+    return arg;
+  };
+}
