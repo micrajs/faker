460387245d113f52c157a7d564d70ddf34ab618d
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-03 14:50:54 -0500

    docs: added docblock to uuid extension

--- extensions/uuid/uuid.register.d.ts
+++ extensions/uuid/uuid.register.d.ts
@@ -1,7 +1,19 @@
 declare global {
   namespace Faker {
     interface Extensions {
-      uuid: () => string;
+      /**
+       * It generates a random uuid.
+       *
+       * @example
+       * ```ts
+       * import fake from '@micra/faker';
+       * import {uuid} from '@micra/faker/extensions/uuid';
+       *
+       * fake.extend({uuid});
+       * fake.uuid(); // 'f8d8e8a4-f9a6-4d0b-b8a4-f8d8e8a4f9a6'
+       * ```
+       */
+      uuid(): string;
     }
   }
 }
--- extensions/uuid/uuid.ts
+++ extensions/uuid/uuid.ts
@@ -1,3 +1,16 @@
+/**
+ * It register the uuid extension to the Faker instance.
+ *
+ * @param fake Faker instance
+ *
+ * @example
+ * ```ts
+ * import fake from '@micra/faker';
+ * import {uuid} from '@micra/faker/extensions/uuid';
+ *
+ * fake.extend({uuid});
+ * ```
+ */
 export function uuid(fake: Faker.Instance) {
   return (): string => {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
