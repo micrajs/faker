eaedb3a495ba4656b53597c7b98f8985427f6bcd
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-02 20:52:03 -0500

    test: added tests for string, boolean and oneOf methods of FakerCore

--- data/classes/tests/FakerCore.test.ts
+++ data/classes/tests/FakerCore.test.ts
@@ -14,4 +14,34 @@ describe('Faker tests', () => {
     expect(fake.seed(1).number()).toBe(fake.seed(1).number());
     expect(fake.seed(1).number()).not.toBe(fake.seed(1).unseed().number());
   });
+
+  it('should generate strings', () => {
+    const fake = new FakerCore();
+
+    expect(typeof fake.string()).toBe('string');
+    expect(fake.string()).toHaveLength(10);
+    expect(fake.string(2)).toHaveLength(2);
+    expect(fake.string()).not.toBe(fake.string());
+    expect(fake.seed(1).string()).toBe(fake.seed(1).string());
+    expect(fake.seed(1).string()).not.toBe(fake.seed(1).unseed().string());
+  });
+
+  it('should generate booleans', () => {
+    const fake = new FakerCore();
+
+    expect(typeof fake.boolean()).toBe('boolean');
+    expect(fake.seed(1).boolean()).toBe(fake.seed(1).boolean());
+  });
+
+  it('should generate oneOf the options', () => {
+    const spy = vi.fn();
+    const fake = new FakerCore();
+
+    fake.oneOf(spy);
+    expect(spy).toHaveBeenCalledWith(fake);
+    expect(fake.oneOf('string')).toBe('string');
+    expect(fake.seed(1).oneOf(24, (fake) => fake.number(0, 42))).toBe(
+      fake.seed(1).oneOf(24, (fake) => fake.number(0, 42)),
+    );
+  });
 });
