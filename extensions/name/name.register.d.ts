ed782b63cdafccb0f522bd903fe8114deff08218
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-03 14:31:08 -0500

    docs: added docblock to name extension

--- extensions/name/name.register.d.ts
+++ extensions/name/name.register.d.ts
@@ -3,11 +3,106 @@ declare global {
     type BinaryGender = 'male' | 'female';
 
     interface Extensions {
-      name: (gender?: BinaryGender) => string;
-      firstName: (gender?: BinaryGender) => string;
-      lastName: () => string;
-      nameSuffix: () => string;
-      namePrefix: () => string;
+      /**
+       * It generates a random full name.
+       *
+       * @example
+       * ```ts
+       * import fake from '@micra/faker';
+       * import {name} from '@micra/faker/extensions/name';
+       *
+       * fake.extend({name});
+       * fake.name(); // 'John Smith'
+       * ```
+       */
+      name(): string;
+      /**
+       * It generates a random full name based on a binary gender.
+       *
+       * @param binaryGender - Binary gender string.
+       *
+       * @example
+       * ```ts
+       * import fake from '@micra/faker';
+       * import {name} from '@micra/faker/extensions/name';
+       *
+       * fake.extend({name});
+       * fake.name('female'); // 'Jane Doe'
+       * ```
+       */
+      name(binaryGender: BinaryGender): string;
+      name(binaryGender?: BinaryGender): string;
+
+      /**
+       * It generates a random first name.
+       *
+       * @example
+       * ```ts
+       * import fake from '@micra/faker';
+       * import {firstName} from '@micra/faker/extensions/name';
+       *
+       * fake.extend({firstName});
+       * fake.firstName(); // 'John'
+       * ```
+       */
+      firstName(): string;
+      /**
+       * It generates a random first name based on a binary gender.
+       *
+       * @param binaryGender - Binary gender
+       *
+       *  @example
+       * ```ts
+       * import fake from '@micra/faker';
+       * import {firstName} from '@micra/faker/extensions/name';
+       *
+       * fake.extend({firstName});
+       * fake.firstName('female'); // 'Jane'
+       * ```
+       */
+      firstName(binaryGender: BinaryGender): string;
+      firstName(binaryGender?: BinaryGender): string;
+
+      /**
+       * It generates a random last name.
+       *
+       * @example
+       * ```ts
+       * import fake from '@micra/faker';
+       * import {lastName} from '@micra/faker/extensions/name';
+       *
+       * fake.extend({lastName});
+       * fake.lastName(); // 'Smith'
+       * ```
+       */
+      lastName(): string;
+
+      /**
+       * It generates a random name suffix.
+       *
+       * @example
+       * ```ts
+       * import fake from '@micra/faker';
+       * import {nameSuffix} from '@micra/faker/extensions/name';
+       *
+       * fake.extend({nameSuffix});
+       * fake.nameSuffix(); // 'Jr.'
+       * ```
+       */
+      nameSuffix(): string;
+
+      /**
+       * It generates a random name prefix.
+       *
+       * @example
+       * ```ts
+       * import fake from '@micra/faker';
+       * import {namePrefix} from '@micra/faker/extensions/name';
+       *
+       * fake.extend({namePrefix});
+       * fake.namePrefix(); // 'Mr.'
+       */
+      namePrefix(): string;
     }
   }
 }
--- extensions/name/name.ts
+++ extensions/name/name.ts
@@ -5,8 +5,21 @@ import {nameSuffixes} from '@/data/data-sources/nameSuffixes';
 import {namePrefixes} from '@/data/data-sources/namePrefixes';
 import {lastNames} from '@/data/data-sources/lastNames';
 
+/**
+ * It register the name extension to the Faker instance.
+ *
+ * @param fake Faker instance
+ *
+ * @example
+ * ```ts
+ * import fake from '@micra/faker';
+ * import {name} from '@micra/faker/extensions/name';
+ *
+ * fake.extend({name});
+ * ```
+ */
 export function name(fake: Faker.Instance): Faker.Extensions['name'] {
-  return (gender): string => {
+  return (gender?: Faker.BinaryGender): string => {
     if (gender === 'female') {
       return `${fake.oneOf(femaleFirstNames)} ${fake.oneOf(lastNames)}`;
     }
@@ -42,8 +55,21 @@ export function name(fake: Faker.Instance): Faker.Extensions['name'] {
   };
 }
 
+/**
+ * It register the firstName extension to the Faker instance.
+ *
+ * @param fake Faker instance
+ *
+ * @example
+ * ```ts
+ * import fake from '@micra/faker';
+ * import {firstName} from '@micra/faker/extensions/name';
+ *
+ * fake.extend({firstName});
+ * ```
+ */
 export function firstName(fake: Faker.Instance): Faker.Extensions['firstName'] {
-  return (gender): string => {
+  return (gender?: Faker.BinaryGender): string => {
     if (gender === 'female') {
       return fake.oneOf(femaleFirstNames);
     }
@@ -56,12 +82,38 @@ export function firstName(fake: Faker.Instance): Faker.Extensions['firstName'] {
   };
 }
 
+/**
+ * It register the lastName extension to the Faker instance.
+ *
+ * @param fake Faker instance
+ *
+ * @example
+ * ```ts
+ * import fake from '@micra/faker';
+ * import {lastName} from '@micra/faker/extensions/name';
+ *
+ * fake.extend({lastName});
+ * ```
+ */
 export function lastName(fake: Faker.Instance): Faker.Extensions['lastName'] {
   return (): string => {
     return fake.oneOf(lastNames);
   };
 }
 
+/**
+ * It register the namePrefix extension to the Faker instance.
+ *
+ * @param fake Faker instance
+ *
+ * @example
+ * ```ts
+ * import fake from '@micra/faker';
+ * import {namePrefix} from '@micra/faker/extensions/name';
+ *
+ * fake.extend({namePrefix});
+ * ```
+ */
 export function namePrefix(
   fake: Faker.Instance,
 ): Faker.Extensions['namePrefix'] {
@@ -70,6 +122,19 @@ export function namePrefix(
   };
 }
 
+/**
+ * It register the nameSuffix extension to the Faker instance.
+ *
+ * @param fake Faker instance
+ *
+ * @example
+ * ```ts
+ * import fake from '@micra/faker';
+ * import {nameSuffix} from '@micra/faker/extensions/name';
+ *
+ * fake.extend({nameSuffix});
+ * ```
+ */
 export function nameSuffix(
   fake: Faker.Instance,
 ): Faker.Extensions['nameSuffix'] {
